f016a6cada0f83700d380bd5fe50bafa696c2da7
hakimryan <ryan.s.hakim@gmail.com>
2015-03-19 14:04:22 +0700

    first
    Signed-off-by: hakimryan <ryan.s.hakim@gmail.com>

---
+++ rule184.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var center, left, right int
+	n := 20
+	world := make([]int, n)
+	currentWorld := make([]int, n)
+
+	// set initial state
+	world[3] = 1
+	world[4] = 1
+	world[6] = 1
+	world[7] = 1
+	world[9] = 1
+	world[10] = 1
+	world[11] = 1
+	fmt.Println(world)
+
+	T := 20
+
+	for j := 0; j < T; j++ {
+		copy(currentWorld, world)
+		for i := 0; i < n; i++ {
+			if i == 0 {
+				left = currentWorld[n-1]
+			} else {
+				left = currentWorld[i-1]
+			}
+			if i == n-1 {
+				right = currentWorld[0]
+			} else {
+				right = currentWorld[i+1]
+			}
+			center = currentWorld[i]
+
+			switch {
+			case left == 0 && center == 0 && right == 0:
+				world[i] = 0
+			case left == 0 && center == 0 && right == 1:
+				world[i] = 0
+			case left == 0 && center == 1 && right == 0:
+				world[i] = 0
+			case left == 0 && center == 1 && right == 1:
+				world[i] = 1
+			case left == 1 && center == 0 && right == 0:
+				world[i] = 1
+			case left == 1 && center == 0 && right == 1:
+				world[i] = 1
+			case left == 1 && center == 1 && right == 0:
+				world[i] = 0
+			case left == 1 && center == 1 && right == 1:
+				world[i] = 1
+			}
+		}
+		// fmt.Println(currentWorld)
+		fmt.Println(world)
+	}
+
+}
